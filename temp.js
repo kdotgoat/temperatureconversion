f80124dab68898d7fd6341fe69afafc944a3b13b
Timona orado <wisem9086@gmail.com>
2024-09-24 22:44:21 +0300

    Miss me?

---
+++ temp.js
@@ -0,0 +1,21 @@
+const textBox = document.getElementById("text_box");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+let temp;
+function convert(){
+if(toFahrenheit.checked){
+temp = Number(textBox.value);
+temp = temp * 9 / 5 + 32;
+result.textContent=temp.toFixed(1) + "ºF";
+}
+else if(toCelsius.checked){
+    temp = Number(textBox.value);
+    temp = (temp - 32) * (5/9);
+    result.textContent=temp.toFixed(1) + "ºC";
+}
+else{
+    result.textContent=`Select a unit`;
+}
+}
